f8ac23efc9c2ef073f39609a8bcee4a1250a889b
tuvalBarakWSC <tuval.barak@wsc-sports.com>
2025-01-29 15:59:20 +0200

    Version 1.4.3

--- app/src/main/java/com/wscsports/android/blaze/sampleapp/ads/gam/CustomNativeAdsDelegate.kt
+++ app/src/main/java/com/wscsports/android/blaze/sampleapp/ads/gam/CustomNativeAdsDelegate.kt
@@ -5,16 +5,21 @@ import android.util.Log
 import com.blaze.blazesdk.ads.custom_native.BlazeGoogleCustomNativeAdsHandler
 import com.blaze.gam.custom_native.BlazeCustomNativeAdData
 import com.blaze.gam.custom_native.BlazeGAMCustomNativeAdsDelegate
+import com.blaze.gam.custom_native.BlazeGamCustomNativeAdRequestInformation
 
 object CustomNativeAdsDelegate : BlazeGAMCustomNativeAdsDelegate {
 
-    override val customGAMTargetingProperties: Map<String, String> = emptyMap()
+    override fun customGAMTargetingProperties(requestData: BlazeGamCustomNativeAdRequestInformation): Map<String, String> {
+        return emptyMap()
+    }
 
-    override val publisherProvidedId: String?
-        get() = null
+    override fun publisherProvidedId(requestData: BlazeGamCustomNativeAdRequestInformation): String? {
+        return null
+    }
 
-    override val networkExtras: Bundle?
-        get() = null
+    override fun networkExtras(requestData: BlazeGamCustomNativeAdRequestInformation): Bundle? {
+        return null
+    }
 
     override fun onGAMCustomNativeAdError(errMsg: String) {
         Log.d("CustomNativeAdsDelegate", "onGAMCustomNativeAdError: Error on ad - $errMsg")
