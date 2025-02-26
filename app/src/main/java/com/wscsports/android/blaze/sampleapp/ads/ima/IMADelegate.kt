2d53243002ce4e66b40321e3beb2d47a28b2fc25
Asaf Ben Artzy <asaf.benartzi@wsc-sprorts.com>
2025-02-26 15:23:46 +0200

    v1.7.1 updates

--- app/src/main/java/com/wscsports/android/blaze/sampleapp/ads/ima/IMADelegate.kt
+++ app/src/main/java/com/wscsports/android/blaze/sampleapp/ads/ima/IMADelegate.kt
@@ -3,18 +3,19 @@ package com.wscsports.android.blaze.sampleapp.ads.ima
 import android.util.Log
 import com.blaze.blazesdk.ads.ima.BlazeIMAHandlerEventType
 import com.blaze.blazesdk.ads.ima.models.BlazeImaAdInfo
+import com.blaze.ima.BlazeIMAAdRequestInformation
 import com.blaze.ima.BlazeIMADelegate
 import com.google.ads.interactivemedia.v3.api.ImaSdkFactory
 import com.google.ads.interactivemedia.v3.api.ImaSdkSettings
 
 object IMADelegate : BlazeIMADelegate {
 
-    override fun additionalIMATagQueryParams(): Map<String, String> {
+    override fun additionalIMATagQueryParams(requestData: BlazeIMAAdRequestInformation): Map<String, String> {
         // Any additional query params
         return emptyMap()
     }
 
-    override fun customIMASettings(): ImaSdkSettings {
+    override fun customIMASettings(requestData: BlazeIMAAdRequestInformation): ImaSdkSettings? {
         // Optional implementation. You can use this to customize ImaSdkSettings.
         return ImaSdkFactory.getInstance().createImaSdkSettings().apply {
 
@@ -29,7 +30,7 @@ object IMADelegate : BlazeIMADelegate {
         Log.d("onIMAAdEvent", "eventType - ${eventType.name}, adInfo - $adInfo")
     }
 
-    override fun overrideAdTagUrl(): String? {
+    override fun overrideAdTagUrl(requestData: BlazeIMAAdRequestInformation): String? {
         // Optional implementation. You can use this to override the ad tag URL.
         return null
     }
